34c83c07040fd1646c599c5a2cf1d242f85c175e
Navishh <navishkanuwan11@gmail.com>
2024-10-25 10:03:53 +0530

    chore: minor fix

--- actions/register.ts
+++ actions/register.ts
@@ -9,7 +9,7 @@ import * as z from "zod";
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
-  if (!validatedFields) {
+  if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
 
