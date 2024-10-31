c514379e486b2b818162eaddc0a8caa7d7cbebbb
Navishh <navishkanuwan11@gmail.com>
2024-10-31 17:19:31 +0530

    chore: minor fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,6 @@
 // import { handlers } from "@/auth"; // Referring to the auth.ts we just created
-export { GET, POST } from "@/auth";
+// export const { GET, POST } = handlers;
+// export { GET, POST } from "@/auth";
+// import { handlers } from "@/auth"; // Referring to the auth.ts we just created
+import { handlers } from "@/auth"; // Referring to the auth.ts we just created
+export const { GET, POST } = handlers;
