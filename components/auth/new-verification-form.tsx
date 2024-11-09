54940dd8dccd9a2f1866e48b5b49c28ee5bc787f
Navishh <navishkanuwan11@gmail.com>
2024-11-09 18:28:06 +0530

    chore: minor fix

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,24 +1,56 @@
 "use client";
 import { newVerification } from "@/actions/new-verification";
 import { useSearchParams } from "next/navigation";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { HashLoader } from "react-spinners";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
 import { CardWrapper } from "./card-wrapper";
 
 export const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+
+  const [success, setSuccess] = useState<string | undefined>();
+
   const searchParams = useSearchParams();
 
   const token = searchParams.get("token");
 
+  // const onSubmit = useCallback(() => {
+  //   if (!token) {
+  //     setError("Missing Token!");
+  //     return;
+  //   }
+
+  //   newVerification(token);
+  //   .then((data) => {
+  //     setSuccess(data.success);
+  //     setError(data.error);
+  //   })
+  // }, [token]);
+
+  // useEffect(() => {
+  //   onSubmit();
+  // }, [onSubmit]);
   const onSubmit = useCallback(() => {
-    if (!token) return;
-    newVerification(token);
+    if (!token) {
+      setError("Missing Token!");
+      return;
+    }
+
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch(() => {
+        setError("Something went wrong!");
+      });
   }, [token]);
 
   useEffect(() => {
     onSubmit();
   }, [onSubmit]);
-
   return (
     <CardWrapper
       headerLabel="Confirming your verification"
@@ -26,7 +58,9 @@ export const NewVerificationForm = () => {
       backButtonHref="/auth/login"
     >
       <div className="flex items-center w-full justify-center">
-        <HashLoader size={20} color="orange" />
+        {!success && !error && <HashLoader size={20} color="orange" />}
+        <FormSuccess message={success} />
+        <FormError message={error} />
       </div>
     </CardWrapper>
   );
