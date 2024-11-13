6b986419abdda303be6c00a804228377c11efb96
Navishh <navishkanuwan11@gmail.com>
2024-11-13 21:40:42 +0530

    update: update reset password page

---
+++ components/auth/reset-form.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { reset } from "@/actions/reset";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { ResetSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { CardWrapper } from "./card-wrapper";
+
+export const ResetForm = () => {
+  // const errorParam = Array.isArray(searchParams.error)
+  //   ? searchParams.error[0]
+  //   : searchParams.error;
+  // const urlError =
+  //   errorParam === "OAuthAccountNotLinked"
+  //     ? "Email is already in use with a different provider!"
+  //     : "";
+
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof ResetSchema>>({
+    resolver: zodResolver(ResetSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof ResetSchema>) => {
+    setError("");
+    setSuccess("");
+
+    console.log(values);
+    startTransition(() => {
+      reset(values).then((data) => {
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel="Forgot your password?"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="navishkanuwan11@gmail.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button disabled={isPending} className="w-full " type="submit">
+            Send reset email{" "}
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
