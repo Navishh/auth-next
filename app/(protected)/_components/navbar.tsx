80e8a8d8b8c1001d914f38d8e627a4c742716710
Navishh <navishkanuwan11@gmail.com>
2024-11-22 20:30:07 +0530

    feat:implement navbar

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -1,16 +1,53 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export const Navbar = () => {
+  const pathaname = usePathname();
+
   return (
-    <div className="fixed top-0 left-0 flex justify-between items-center w-full px-10 py-4 bg-[#1f1f1f] text-white">
-      <div className="flex items-center space-x-4">
-        <h1 className="text-lg font-bold">LOGO.</h1>
+    <nav className="fixed top-0 left-0 flex justify-between items-center w-full px-10 py-4 bg-[#1f1f1f] text-white">
+      {/* <div className="flex items-center space-x-4 gap-x-2">
+        <h1 className="text-lg font-bold">
+          <a href="/settings" className="text-white">
+            LOGO.
+          </a>
+        </h1>
       </div>
       <div className="flex items-center space-x-4">
-        <a href="/protected/settings" className="text-white">
+        <a href="/settings" className="text-white">
           Settings
         </a>
+      </div> */}
+      <div className=" flex gap-x-2 ">
+        <Button
+          asChild
+          variant={pathaname === "/server" ? "default" : "outline"}
+        >
+          <Link href="/server">Server</Link>
+        </Button>
+        <Button
+          asChild
+          variant={pathaname === "/client" ? "default" : "outline"}
+        >
+          <Link href="/client">Client</Link>
+        </Button>
+        <Button
+          asChild
+          variant={pathaname === "/admin" ? "default" : "outline"}
+        >
+          <Link href="/admin">Admin</Link>
+        </Button>
+        <Button
+          asChild
+          variant={pathaname === "/settings" ? "default" : "outline"}
+        >
+          <Link href="/settings">Settings</Link>
+        </Button>
       </div>
-    </div>
+      <p>User Butoon</p>
+    </nav>
   );
 };
