59bfa488f14286bebc431a5d39e0fe8f3d1ad2fa
Navishh <navishkanuwan11@gmail.com>
2024-11-24 16:36:00 +0530

    feat: create server and client pages

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { UserInfo } from "@/components/user-info";
+import { useCurrenUser } from "@/hooks/use-current-user";
+
+const ClientPage = () => {
+  const user = useCurrenUser();
+  return (
+    <>
+      <p className="relative overflow-hidden bg-blue-200 text-slate-500 font-medium px-4 py-2 rounded-md transition-all duration-300 group">
+        <span className="absolute inset-0 bg-blue-500 transform -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-in-out"></span>
+        <span className="relative z-10 transition-colors duration-500 group-hover:text-white">
+          Client{" "}
+        </span>
+      </p>
+      <UserInfo label="🥸 User Details" user={user} />
+    </>
+  );
+};
+
+export default ClientPage;
