26e94f0a6a31ca5bd2f5ab103807c8525b2c41fd
Navishh <navishkanuwan11@gmail.com>
2024-11-30 16:39:19 +0530

    update: implement 2fa switch

--- actions/settings.ts
+++ actions/settings.ts
@@ -1,8 +1,10 @@
 "use server";
 
-import { getUserById } from "@/data/user";
+import { getUserByEmail, getUserById } from "@/data/user";
 import { currentUser } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { sendVerificationEmail } from "@/lib/mail";
+import { generateVerificationToken } from "@/lib/tokens";
 import { SettingsSchema } from "@/schemas";
 import * as z from "zod";
 
@@ -22,6 +24,34 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     return { error: "Unautherized" };
   }
 
+  if (user.isOAuth) {
+    values.email = undefined;
+    values.password = undefined;
+    values.newPassword = undefined;
+    values.newPassword = undefined;
+    values.isTwoFactorEnabled = undefined;
+  }
+
+  if (values.email && values.email !== user.email) {
+    const exitingUser = await getUserByEmail(values.email);
+
+    if (exitingUser && exitingUser.id !== user.id) {
+      return { error: "Email already in use" };
+    }
+  }
+
+  // const verificationToken = values.email ? await generateVerificationToken(values.email) : null;
+
+  // if (verificationToken) {
+  //   await sendVerificationEmail(
+  //     verificationToken.email,
+  //     verificationToken.token
+  //   );
+  // }
+
+  const verificatioToken = await generateVerificationToken(values.email || "");
+
+  await sendVerificationEmail(verificatioToken.email, verificatioToken.token);
   await db.user.update({
     where: { id: dbUser.id },
     data: {
